e32651b30e7bfb8d85cb5c899babdf8d296a7eac
Miles Dowe <mdowe@hollandamerica.com>
2017-02-10 15:59:36 -0800

    Update metadata form

--- app/viewer/instanceList/metadataForm/metadata-form.component.js
+++ app/viewer/instanceList/metadataForm/metadata-form.component.js
@@ -4,5 +4,12 @@ angular.module('laughResearchApp.viewer')
     templateUrl: 'app/viewer/instanceList/metadataForm/metadata-form.html',
     controller: function MetadataFormController($scope) {
         // maybe some logic to POST to web service
+
+        $scope.hideSpeakerField = true;
+
+        $scope.disableSpeakerField = function (bool) {
+            document.getElementById('speaker').disabled = bool;
+            $scope.hideSpeakerField = bool;
+        }
     }
 });
